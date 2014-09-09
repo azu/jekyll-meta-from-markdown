80d55e6e55e3b264a13081116b732360fba09c04
azu <azuciao@gmail.com>
2014-09-09 22:21:21 +0900

    feat(options): support `author`

--- test/fixtures/output.markdown
+++ test/fixtures/output.markdown
@@ -1,6 +1,7 @@
 ---
 layout: post
 title: "title"
+author: azu
 date: 2014-09-08T21:38:00.000Z
 ---
 
--- test/jekyll-meta-from-markdown-test.js
+++ test/jekyll-meta-from-markdown-test.js
@@ -11,14 +11,15 @@ describe("jekyll-meta-from-markdown-test", function () {
         var input = fs.readFileSync(__dirname + "/fixtures/input.markdown", "utf-8");
         var output = fs.readFileSync(__dirname + "/fixtures/output.markdown", "utf-8");
         it("should return text contain jekyll front matter", function () {
-            assert.equal(jekyllMatter(input,{
-                date : new Date("2014-09-08T21:38")
+            assert.equal(jekyllMatter(input, {
+                author: "azu",
+                date: new Date("2014-09-08T21:38")
             }), output);
         });
     });
     describe("when input text has not title", function () {
         it("should occur exception", function () {
-            assert.throws(function(){
+            assert.throws(function () {
                 jekyllMatter("no title text");
             });
         });
